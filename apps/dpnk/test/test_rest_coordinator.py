4bfed775bb822598c7b3b21c8f89c4ca14b8afe6
Michal Klimpera <michal.klimpera@gmail.com>
2025-12-18 21:42:08 +0100

    apps/dpnk: add /coordinator/approve-payments REST API endpoint including tests

--- apps/dpnk/test/test_rest_coordinator.py
+++ apps/dpnk/test/test_rest_coordinator.py
@@ -12,7 +12,11 @@ from dpnk import util
 
 from dpnk.models import (
     UserAttendance,
+    UserProfile,
+    Status,
 )
+
+import json
 
 
 @override_settings(
@@ -59,3 +63,57 @@ class FeeApprovalSetTest(TestCase):
                 ],
             },
         )
+
+
+@override_settings(
+    SITE_ID=2,
+    FAKE_DATE=datetime.date(year=2025, month=1, day=10),
+)
+class ApprovePaymentsViewTest(TestCase):
+
+    fixtures = [
+        "dump",
+    ]
+
+    def setUp(self):
+        super().setUp()
+        self.client = APIClient(
+            HTTP_HOST="testing-campaign.testserver", HTTP_REFERER="test-referer"
+        )
+        self.maxDiff = None
+
+    def test_post(self):
+        self.client.force_login(
+            User.objects.get(pk=3), settings.AUTHENTICATION_BACKENDS[0]
+        )
+        post_data = {
+            "ids": [5, 4],
+        }
+        response = self.client.post(
+            reverse("approve-payments"), post_data, format="json", follow=True
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(
+            response.content.decode(),
+            {
+                "message": "Approved 1 payments successfully",
+                "approved_ids": [5],
+            },
+        )
+
+        user_attendance = UserAttendance.objects.get(pk=5)
+        payment = user_attendance.representative_payment
+        self.assertEqual(payment.status, Status.COMPANY_ACCEPTS)
+
+    def test_permissions(self):
+        self.client.force_login(
+            User.objects.get(pk=1), settings.AUTHENTICATION_BACKENDS[0]
+        )
+        post_data = {
+            "ids": [5, 4],
+        }
+        response = self.client.post(
+            reverse("approve-payments"), post_data, format="json", follow=True
+        )
+        self.assertEqual(response.status_code, 403)
--- apps/dpnk/urls.py
+++ apps/dpnk/urls.py
@@ -30,7 +30,7 @@ from .autocomplete_views import (
     SubsidiaryAutocomplete,
     TeamAutocomplete,
 )
-from .rest_coordinator import router as coordinator_router
+from .rest_coordinator import ApprovePaymentsView, router as coordinator_router
 from .views import (
     answers,
     questionnaire_answers,
@@ -595,5 +595,10 @@ urlpatterns = [
         "rest/coordinator/",
         include(coordinator_router.urls),
         name="coordinator_rest_api",
+    ),
+    path(
+        "rest/coordinator/approve-payments/",
+        ApprovePaymentsView.as_view(),
+        name="approve-payments",
     ),
 ]
