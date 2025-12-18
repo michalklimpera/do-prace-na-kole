77fcec86f43f06bc6c7843f8bb510ebc179a9052
Michal Klimpera <michal.klimpera@gmail.com>
2025-12-18 22:46:06 +0100

    apps/dpnk: add /coordinator/subsidiary (GET), /coordinator/subsidiary/{id}/team (GET, PATCH, DELETE), /coordinator/subsidiary/{id}/team/{id}/member (GET, PATCH, DELETE) REST API endpoints including tests

--- apps/dpnk/rest_coordinator.py
+++ apps/dpnk/rest_coordinator.py
@@ -10,15 +10,20 @@ from .models import (
     Payment,
     Status,
     Company,
+    Subsidiary,
+    Team,
+    Campaign,
 )
 
 from .models.company import CompanyInCampaign
 from .rest import (
     AddressSerializer,
     EmptyStrField,
+    RequestSpecificField,
 )
 from .middleware import get_or_create_userattendance
 import datetime
+from rest_framework.exceptions import APIException
 
 
 class UserAttendanceMixin:
@@ -43,6 +48,12 @@ class FeeApprovalSerializer(serpy.Serializer):
 class CompanyAdminDoesNotExist(serializers.ValidationError):
     status_code = 403
     default_detail = {"company_admin": "User is not company admin"}
+
+
+class TeamNotEmpty(APIException):
+    status_code = 400
+    default_detail = "Cannot delete a team with active members."
+    default_code = "team_not_empty"
 
 
 class FeeApprovalSet(viewsets.ReadOnlyModelViewSet, UserAttendanceMixin):
@@ -200,6 +211,190 @@ class GetAttendanceView(APIView, UserAttendanceMixin):
     permission_classes = [permissions.IsAuthenticated]
 
 
+class TeamNameSerializer(serpy.Serializer):
+    id = serpy.IntField()
+    name = serpy.StrField()
+
+
+class SubsidiarySerializer(serpy.Serializer):
+    id = serpy.IntField()
+    address = AddressSerializer()
+    city = serpy.StrField()
+    teams = RequestSpecificField(
+        lambda subsidiary, req: [
+            TeamNameSerializer(t).data
+            for t in subsidiary.teams.filter(
+                campaign__slug=req.subdomain,
+            )
+        ]
+    )
+
+
+class SubsidiaryTeamSerializer(serpy.Serializer):
+    id = serpy.IntField()
+    name = serpy.StrField()
+    members = UserAttendanceSerializer(many=True)
+
+
+class SubsidiaryView(viewsets.ReadOnlyModelViewSet, UserAttendanceMixin):
+    def get_queryset(self):
+
+        try:
+            company_admin = CompanyAdmin.objects.get(
+                userprofile=self.ua().userprofile.pk,
+                campaign__slug=self.request.subdomain,
+                company_admin_approved="approved",
+            )
+        except CompanyAdmin.DoesNotExist:
+            raise CompanyAdminDoesNotExist
+
+        queryset = Subsidiary.objects.filter(
+            company=company_admin.administrated_company,
+        )
+
+        return queryset
+
+    serializer_class = SubsidiarySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class SubsidiaryTeamDeserializer(serializers.HyperlinkedModelSerializer):
+    campaign_id = serializers.PrimaryKeyRelatedField(
+        queryset=Campaign.objects.all(), source="campaign"
+    )
+    subsidiary_id = serializers.PrimaryKeyRelatedField(
+        queryset=Subsidiary.objects.all(), source="subsidiary"
+    )
+
+    class Meta:
+        model = Team
+        fields = ("id", "name", "campaign_id", "subsidiary_id")
+
+    def to_representation(self, value):
+        data = super().to_representation(value)
+        del data["campaign_id"]
+        del data["subsidiary_id"]
+        return data
+
+
+class TeamView(viewsets.ModelViewSet, UserAttendanceMixin):
+    def get_queryset(self):
+
+        try:
+            company_admin = CompanyAdmin.objects.get(
+                userprofile=self.ua().userprofile.pk,
+                campaign__slug=self.request.subdomain,
+                company_admin_approved="approved",
+            )
+        except CompanyAdmin.DoesNotExist:
+            raise CompanyAdminDoesNotExist
+
+        queryset = Team.objects.filter(
+            subsidiary__company=company_admin.administrated_company,
+            subsidiary=self.kwargs["subsidiary_id"],
+            campaign__slug=self.request.subdomain,
+        )
+
+        return queryset
+
+    def destroy(self, request, *args, **kwargs):
+        team = self.get_object()
+        if team.all_members().exists():
+            raise TeamNotEmpty
+        return super().destroy(request, *args, **kwargs)
+
+    def get_serializer_class(self):
+        if self.action in ["retrieve", "list"]:
+            return SubsidiaryTeamSerializer
+        else:
+            return SubsidiaryTeamDeserializer
+
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class MemberSerializer(serpy.Serializer):
+    id = serpy.IntField()
+    name = serpy.StrField(call=True)
+    approved_for_team = serpy.StrField()
+
+
+class MemberDeserializer(serializers.HyperlinkedModelSerializer):
+    campaign_id = serializers.PrimaryKeyRelatedField(
+        queryset=Campaign.objects.all(), source="campaign"
+    )
+    team_id = serializers.PrimaryKeyRelatedField(
+        queryset=Team.objects.all(), source="team"
+    )
+
+    class Meta:
+        model = UserAttendance
+        fields = ("id", "name", "team_id", "campaign_id", "approved_for_team")
+
+    def validate(self, data):
+        instance = self.instance or self.Meta.model(**data)
+        if self.instance:
+            old_instance = instance
+            new_data = data
+
+            if "team" in new_data:
+                oldcompany = old_instance.team.subsidiary.company.id
+                newcompany = new_data["team"].subsidiary.company.id
+                if oldcompany != newcompany:
+                    raise serializers.ValidationError(
+                        "Cannot move members between companies"
+                    )
+
+            for key, value in data.items():
+                setattr(instance, key, value)
+        # Call full_clean() to trigger model validation
+        instance.full_clean()
+
+        return data
+
+
+class MemberView(viewsets.ModelViewSet, UserAttendanceMixin):
+    def get_queryset(self):
+
+        try:
+            company_admin = CompanyAdmin.objects.get(
+                userprofile=self.ua().userprofile.pk,
+                campaign__slug=self.request.subdomain,
+                company_admin_approved="approved",
+            )
+        except CompanyAdmin.DoesNotExist:
+            raise CompanyAdminDoesNotExist
+
+        queryset = UserAttendance.objects.filter(
+            team__subsidiary__company=company_admin.administrated_company,
+            team__subsidiary=self.kwargs["subsidiary_id"],
+            team=self.kwargs["team_id"],
+            campaign__slug=self.request.subdomain,
+        )
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action in ["retrieve", "list"]:
+            return MemberSerializer
+        else:
+            return MemberDeserializer
+
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_destroy(self, instance):
+        instance.team = None
+        instance.save()
+
+
 router = routers.DefaultRouter()
 router.register(r"fee-approval", FeeApprovalSet, basename="fee-approval")
 # router.register(r"approve-payments", ApprovePaymentsView, basename="approve-payments")
+router.register(r"subsidiary", SubsidiaryView, basename="subsidiary-coordinator")
+router.register(
+    r"subsidiary/(?P<subsidiary_id>\d+)/team", TeamView, basename="subsidiary-team"
+)
+router.register(
+    r"subsidiary/(?P<subsidiary_id>\d+)/team/(?P<team_id>\d+)/member",
+    MemberView,
+    basename="subsidiary-team-member",
+)
